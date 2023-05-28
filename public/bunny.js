1f05e6815e9a1959889daec4c56d2f1c947bbbc5
Noor Fakih <nfakih@stanford.edu>
2023-05-27 17:22:55 -0700

    about to layer clothing ontop

---
+++ public/bunny.js
@@ -0,0 +1,11 @@
+export default class Bunny {
+  constructor() {
+    this.bg = "images/bg/bg0.png";
+    this.outfit = "images/outfit/o0.png";
+    this.extra = "images/extra/a0.png";
+  }
+
+  toString() {
+    return this.bg;
+  }
+}
