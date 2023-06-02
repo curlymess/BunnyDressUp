7a497300a0f8d7ae2dcf64a4e35bd8d014582f9b
Noor Fakih <nfakih@stanford.edu>
2023-06-01 17:45:42 -0700

    made clothes and extras work w class

--- public/js/app.js
+++ public/js/app.js
@@ -8,6 +8,8 @@ export default class App {
     this.user = null;
     console.log('in here');
     this._updateBg = this._updateBg.bind(this);
+    this._updateClothes = this._updateClothes.bind(this);
+    this._updateExtra = this._updateExtra.bind(this);
 
     /// adds 'click' eventlistener to all elements w the css class 
     document.addEventListener('click', event => {
@@ -18,6 +20,24 @@ export default class App {
           this._updateBg(event, event.target.getAttribute('id'));
       }
   });
+
+  document.addEventListener('click', event => {
+    console.log("inside");
+    console.log(event.target)
+    if (event.target.classList.contains('cBttn')) {
+      console.log("done");
+        this._updateClothes(event, event.target.getAttribute('id'));
+    }
+});
+
+document.addEventListener('click', event => {
+  console.log("inside");
+  console.log(event.target)
+  if (event.target.classList.contains('eBttn')) {
+    console.log("done");
+      this._updateExtra(event, event.target.getAttribute('id'));
+  }
+});
   }
 
   loadData(data){
@@ -36,11 +56,24 @@ export default class App {
       event.preventDefault();
       var bgImg = document.getElementById("bgImg");
       bgImg.src = CLOSET.bg[bgSrc];
-      this.bg = bgSrc;
+      this.bunny.bg = bgSrc;
     }
 
-  render() {
-    return this.bg;
-  }
+    _updateClothes(event, imgSrc) {
+      event.preventDefault();
+      console.log("inside function changeClothes");
+      var imgElem = document.getElementById("outfitImg");
+      imgElem.src = CLOSET.clothes[imgSrc];
+      this.bunny.clothes = imgSrc;
+    }
+
+    _updateExtra(event, imgSrc) {
+      event.preventDefault();
+      console.log("inside function changeExtra");
+      var imgElem = document.getElementById("extraImg");
+      imgElem.src = CLOSET.extra[imgSrc];
+      this.bunny.extra = imgSrc;
+    }
+
 }
 window.App = App;
--- public/js/bunny.js
+++ public/js/bunny.js
@@ -1,18 +1,20 @@
+import CLOSET from "./closet.js";
+
 export default class Bunny {
   constructor() {
     this.body = "images/bunny.png";
     this.bg = "images/no.png";
-    this.outfit = "images/no.png";
+    this.clothes = "images/no.png";
     this.extra = "images/no.png";
   }
 
-  updateBg(event, bgSrc) {
-    console.log("clicked");
-      event.preventDefault();
-      var bgImg = document.getElementById("bgImg");
-      bgImg.src = bgSrc;
-      this.bg = bgSrc;
-    }
+  // updateBg(event, bgSrc) {
+  //   console.log("clicked");
+  //     event.preventDefault();
+  //     var bgImg = document.getElementById("bgImg");
+  //     bgImg.src = CLOSET.bg.bgSrc;
+  //     this.bg = bgSrc;
+  //   }
 
   updateOutfit(newItem) {
     this.outfit = newItem;
